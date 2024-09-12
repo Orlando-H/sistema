5a344cb320d3f63e98dfb6a53d03d13bc85441bd
luisillo77D <luisillo77mario@gmail.com>
2024-09-11 19:27:29 -0700

    Refactor player index view and navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    <link href="./output.css" rel="stylesheet">
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
